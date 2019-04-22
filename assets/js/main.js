9988d575bc05bbc3ccaa4491f3566f7f48541b96
Luis Gonzalez <luismgonzalezb@gmail.com>
2019-04-21 22:41:47 -0700

    Layout almost complete, galery working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,33 @@
-console.log("Loaded ...");
+import Glider from "./glider";
+import ScrollReveal from "ScrollReveal";
+require("waypoints/lib/noframework.waypoints");
+
+window.addEventListener("load", () => {
+  const scrollReveal = new ScrollReveal();
+  scrollReveal.reveal(".column-content", {
+    delay: 300,
+    distance: "100px",
+    interval: 100
+  });
+  const us = document.getElementById("us");
+  const header = document.getElementById("header");
+  const waypoint = new Waypoint({
+    element: header,
+    handler: dir => {
+      if (dir === "down") {
+        us.classList.add("sticky");
+        return header.classList.add("sticky");
+      }
+      us.classList.remove("sticky");
+      return header.classList.remove("sticky");
+    }
+  });
+  new Glider(document.getElementById("glider"), {
+    dots: ".dots",
+    rewind: true,
+    arrows: {
+      prev: ".glider-prev",
+      next: ".glider-next"
+    }
+  });
+});
